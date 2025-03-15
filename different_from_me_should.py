72632bc023088867ec26f03aeea4bba9c9436b16
Norm Julian <normvjulian@gmail.com>
2025-03-15 15:15:11 -0600

    black isort etc

--- different_from_me_should.py
+++ different_from_me_should.py
@@ -1,9 +1,8 @@
 import re
 from pathlib import Path
 
-import praw
 import markovify
-
+import praw
 
 POST_LIMIT_PER_SUB = 100
 MIN_POST_OR_COMMENT_LENGTH = 50
@@ -12,16 +11,16 @@ MIN_POST_OR_COMMENT_LENGTH = 50
 OTHER_PEOPLE_SHOULD = "Other people should"
 STATE_SIZE = len(OTHER_PEOPLE_SHOULD)
 FORCED_SEEDS = {
-    f'{OTHER_PEOPLE_SHOULD} do this.\n',
-    f'{OTHER_PEOPLE_SHOULD} be kinder.\n',
-    f'{OTHER_PEOPLE_SHOULD} understand this.\n',
-    f'{OTHER_PEOPLE_SHOULD} work toward this.\n',
-    f'{OTHER_PEOPLE_SHOULD} notice this.\n',
-    f'{OTHER_PEOPLE_SHOULD} try this.\n',
-    f'{OTHER_PEOPLE_SHOULD} do better.\n',
-    f'{OTHER_PEOPLE_SHOULD} get involved.\n',
-    f'{OTHER_PEOPLE_SHOULD} be more aware of this.\n',
-    f'{OTHER_PEOPLE_SHOULD} care about this.\n',
+    f"{OTHER_PEOPLE_SHOULD} do this.\n",
+    f"{OTHER_PEOPLE_SHOULD} be kinder.\n",
+    f"{OTHER_PEOPLE_SHOULD} understand this.\n",
+    f"{OTHER_PEOPLE_SHOULD} work toward this.\n",
+    f"{OTHER_PEOPLE_SHOULD} notice this.\n",
+    f"{OTHER_PEOPLE_SHOULD} try this.\n",
+    f"{OTHER_PEOPLE_SHOULD} do better.\n",
+    f"{OTHER_PEOPLE_SHOULD} get involved.\n",
+    f"{OTHER_PEOPLE_SHOULD} be more aware of this.\n",
+    f"{OTHER_PEOPLE_SHOULD} care about this.\n",
 }
 SENTENCE_GENERATION_ATTEMPTS = 1000
 
@@ -41,7 +40,7 @@ def different_from_me_should():
         with open(subreddit_lists_path / subreddit_list_file.name, "r") as listfile:
             subreddits = {subname.strip() for subname in listfile.readlines()}
             corpuses_by_subreddit_by_subject[subreddit_subject] = {
-                subreddit: '' for subreddit in subreddits
+                subreddit: "" for subreddit in subreddits
             }
 
     # Populate a corpus based on this year's top posts and/or comments
@@ -51,7 +50,9 @@ def different_from_me_should():
         for sub in corpuses_by_subreddit:
             sub_reader = reddit.subreddit(sub)
             # top posts from this subreddit this year
-            for submission in sub_reader.top(time_filter="year", limit=POST_LIMIT_PER_SUB):
+            for submission in sub_reader.top(
+                time_filter="year", limit=POST_LIMIT_PER_SUB
+            ):
                 # Seed the sub corpus with a huge number of 'neutral'-sounding sentences that
                 # start with my desired prefix
                 for seed_sentence in FORCED_SEEDS:
@@ -60,19 +61,24 @@ def different_from_me_should():
                 # I will not bother to keep trying until I reach
                 # POST_LIMIT_PER_SUB and will just settle for what I find.
                 if len(submission.selftext) >= MIN_POST_OR_COMMENT_LENGTH:
-                    newline_delimited_post_text = submission.selftext.replace('.', '\n') + '\n'
+                    newline_delimited_post_text = (
+                        submission.selftext.replace(".", "\n") + "\n"
+                    )
                     corpuses_by_subreddit[sub] += newline_delimited_post_text
 
-            sub_markovifier = markovify.NewlineText(corpuses_by_subreddit[sub], state_size=STATE_SIZE)
+            sub_markovifier = markovify.NewlineText(
+                corpuses_by_subreddit[sub], state_size=STATE_SIZE
+            )
             # Just generate sentences until we get one with desired start, if possible
             for _ in range(SENTENCE_GENERATION_ATTEMPTS):
                 try:
                     sentence = sub_markovifier.make_sentence_with_start(
-                        OTHER_PEOPLE_SHOULD, strict=False)
+                        OTHER_PEOPLE_SHOULD, strict=False
+                    )
                 except:
                     continue
                 if sentence:
-                    print(f'From {subject} subreddit r/{sub}: ' + sentence  + '\n')
+                    print(f"From {subject} subreddit r/{sub}: " + sentence + "\n")
 
 
 if __name__ == "__main__":
