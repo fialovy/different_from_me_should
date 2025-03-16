2b0bf166b5c3f8127b121bed7881dc94dae59d86
Norm Julian <normvjulian@gmail.com>
2025-03-15 22:01:22 -0600

    so many things that just didnt have to be done

---
+++ __init__.py
@@ -0,0 +1,4 @@
+import nltk
+
+# prerequisite
+nltk.download("punkt_tab")
--- different_from_me_should.py
+++ different_from_me_should.py
@@ -1,40 +1,20 @@
 import re
+import random
 
 import markovify
-import nltk
 import praw
 import prawcore
 from nltk.tokenize import sent_tokenize
 
-from subreddits_by_subject import SUBREDDITS_BY_SUBJECT
-
-POST_LIMIT_PER_SUB = 500
-# Per markovify docs (https://pypi.org/project/markovify/#basic-usage), state
-# size is "a number of words the probability of a next word depends on"
-OTHER_PEOPLE_SHOULD = "Other people should"
-STATE_SIZE = len(OTHER_PEOPLE_SHOULD.split(" "))
-FORCED_SEEDS = {
-    f"{OTHER_PEOPLE_SHOULD} do this.",
-    f"{OTHER_PEOPLE_SHOULD} be kinder.",
-    f"{OTHER_PEOPLE_SHOULD} understand this.",
-    f"{OTHER_PEOPLE_SHOULD} work toward this.",
-    f"{OTHER_PEOPLE_SHOULD} notice this.",
-    f"{OTHER_PEOPLE_SHOULD} try this.",
-    f"{OTHER_PEOPLE_SHOULD} do better.",
-    f"{OTHER_PEOPLE_SHOULD} get involved.",
-    f"{OTHER_PEOPLE_SHOULD} be more aware of this.",
-    f"{OTHER_PEOPLE_SHOULD} care about this.",
-    f"{OTHER_PEOPLE_SHOULD} just stop.",
-    f"{OTHER_PEOPLE_SHOULD} stop trying to do this.",
-    f"{OTHER_PEOPLE_SHOULD} stop doing this.",
-    f"{OTHER_PEOPLE_SHOULD} not have to worry about this.",
-    f"{OTHER_PEOPLE_SHOULD} decide for themselves.",
-    f"{OTHER_PEOPLE_SHOULD} figure out the best way to make this work.",
-}
-SENTENCE_GENERATION_ATTEMPTS = 1000
-
-# prerequisite
-nltk.download("punkt_tab")
+from constants import (
+    SUBREDDITS_BY_SUBJECT,
+    POST_LIMIT_PER_SUB,
+    OTHER_PEOPLE_SHOULD,
+    STATE_SIZE,
+    FORCED_SEEDS,
+    SENTENCE_GENERATION_ATTEMPTS,
+    MY_DUMB_INFINITE_LOOP_PREVENTER,
+)
 
 
 # i will make this modular later :P
@@ -53,31 +33,56 @@ def different_from_me_should():
     for subject, corpuses_by_subreddit in corpuses_by_subreddit_by_subject.items():
         print(f"***** SUBJECT: {subject}")
         for sub in corpuses_by_subreddit:
-            # Seed the sub corpus with a huge number of 'arbitrary' sentences that
-            # start with my desired prefix, since I am unlikely to find it in
-            # the wild.
-            for seed_sentence in FORCED_SEEDS:
-                corpuses_by_subreddit_by_subject[subject][sub] += f"{seed_sentence} "
+            all_gathered_text_blobs = []
+            # For each sub, we will also throw in a number of 'arbitrary' sentences that
+            # start with the desired prefix, since it is unlikely to be found directly
+            # in the wild. The hope is that these sound generic enough to follow a
+            # more telling statement that might be found in the posts and comments
+            # (e.g., <some sentence characteristic of the sub> "Other people should
+            # take this to heart.")
             try:
                 sub_reader = reddit.subreddit(sub)
                 # top posts from this subreddit this year
                 for submission in sub_reader.top(
                     time_filter="year", limit=POST_LIMIT_PER_SUB
                 ):
-                    # Then add the submission (post) text to the corpus.
+                    # Then add the submission (post) text to the blobs.
                     # I will not bother to keep trying until I reach
                     # POST_LIMIT_PER_SUB and will just settle for what I find.
                     if submission.selftext:
                         post_sentences = sent_tokenize(submission.selftext)
-                        corpuses_by_subreddit_by_subject[subject][sub] += " ".join(
-                            post_sentences
-                        )
+                        #corpuses_by_subreddit_by_subject[subject][sub] += " ".join(
+                        #    post_sentences
+                        #)
+                        # We want to keep multiple sentences in a row together
+                        # and then randomly stick in the seed things
+                        all_gathered_text_blobs.extend(post_sentences)
             except prawcore.exceptions.Forbidden:
                 print(f"Got 403 when trying to access r/{sub}; skipping")
                 continue
 
+            # Inject seed sentences at random places in the list of gathered
+            # reddit sentences, preferably not next to each other
+            num_blobs = len(all_gathered_text_blobs)
+            seen_neighbor_indices = set()
+            there_just_isnt_enough = 0
+            for seed_sentence in FORCED_SEEDS:
+                index_to_insert_at = random.randrange(num_blobs)
+                while index_to_insert_at in seen_neighbor_indices and there_just_isnt_enough < MY_DUMB_INFINITE_LOOP_PREVENTER:
+                    there_just_isnt_enough += 1
+                    index_to_insert_at = random.randrange(num_blobs)
+                # should increase this window probably
+                seen_neighbor_indices.update({index_to_insert_at - 1, index_to_insert_at, index_to_insert_at + 1})
+                all_gathered_text_blobs.insert(index_to_insert_at, seed_sentence)
+            #for seed_sentence in FORCED_SEEDS:
+            #    all_gathered_text_blobs.append(seed_sentence)
+            #    #corpuses_by_subreddit_by_subject[subject][sub] += f"{seed_sentence} "
+            
+            sub_corpus = " ".join(all_gathered_text_blobs)
+            import pdb; pdb.set_trace() 
             sub_markovifier = markovify.Text(
-                corpuses_by_subreddit_by_subject[subject][sub], state_size=STATE_SIZE
+                #corpuses_by_subreddit_by_subject[subject][sub], state_size=STATE_SIZE
+                sub_corpus, state_size=STATE_SIZE
             )
 
             # Just generate sentences until we get one with desired start, if possible
