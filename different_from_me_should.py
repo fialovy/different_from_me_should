beccba9fef9de37ab6a5469847512b45d4ce75a9
Norm Julian <normvjulian@gmail.com>
2025-03-15 22:17:43 -0600

    getting better

--- different_from_me_should.py
+++ different_from_me_should.py
@@ -17,29 +17,19 @@ from constants import (
 )
 
 
-# i will make this modular later :P
+# i will make this modular later or never :P
 def different_from_me_should():
     # Note that this uses a praw.ini in a separate config directory ($HOME/.config
     # for me on Linux). That is where the dfm_bot argument comes from, as
     # well as the user_agent and authentication creds.
     # See https://praw.readthedocs.io/en/stable/getting_started/configuration/prawini.html
-    corpuses_by_subreddit_by_subject = {
-        subreddit_subject: {
-            subreddit: "" for subreddit in subreddits}
-           for subreddit_subject, subreddits in SUBREDDITS_BY_SUBJECT.items()
-    }
-    # Populate a corpus based on this year's top posts and/or comments
     reddit = praw.Reddit("dfm_bot")
-    for subject, corpuses_by_subreddit in corpuses_by_subreddit_by_subject.items():
+    for subject, subreddits in SUBREDDITS_BY_SUBJECT.items():
         print(f"***** SUBJECT: {subject}")
-        for sub in corpuses_by_subreddit:
+        for sub in subreddits:
+            # Populate a corpus based on the body texts of this year's top posts
+            # in the subreddit
             all_gathered_text_blobs = []
-            # For each sub, we will also throw in a number of 'arbitrary' sentences that
-            # start with the desired prefix, since it is unlikely to be found directly
-            # in the wild. The hope is that these sound generic enough to follow a
-            # more telling statement that might be found in the posts and comments
-            # (e.g., <some sentence characteristic of the sub> "Other people should
-            # take this to heart.")
             try:
                 sub_reader = reddit.subreddit(sub)
                 # top posts from this subreddit this year
@@ -51,18 +41,18 @@ def different_from_me_should():
                     # POST_LIMIT_PER_SUB and will just settle for what I find.
                     if submission.selftext:
                         post_sentences = sent_tokenize(submission.selftext)
-                        #corpuses_by_subreddit_by_subject[subject][sub] += " ".join(
-                        #    post_sentences
-                        #)
-                        # We want to keep multiple sentences in a row together
-                        # and then randomly stick in the seed things
                         all_gathered_text_blobs.extend(post_sentences)
             except prawcore.exceptions.Forbidden:
                 print(f"Got 403 when trying to access r/{sub}; skipping")
                 continue
 
-            # Inject seed sentences at random places in the list of gathered
-            # reddit sentences, preferably not next to each other
+            # For each sub, we will also inject a number of 'arbitrary' sentences that
+            # start with the desired prefix, since it is unlikely to be found directly
+            # in the wild but markovify requires it to exist in the text.
+            # The hope is that these sound generic enough to follow a
+            # more telling statement that might be found in the posts and comments
+            # (e.g., <some sentence characteristic of the sub> "Other people should
+            # take this to heart.")
             num_blobs = len(all_gathered_text_blobs)
             seen_neighbor_indices = set()
             there_just_isnt_enough = 0
@@ -71,20 +61,17 @@ def different_from_me_should():
                 while index_to_insert_at in seen_neighbor_indices and there_just_isnt_enough < MY_DUMB_INFINITE_LOOP_PREVENTER:
                     there_just_isnt_enough += 1
                     index_to_insert_at = random.randrange(num_blobs)
-                # should increase this window probably
-                seen_neighbor_indices.update({index_to_insert_at - 1, index_to_insert_at, index_to_insert_at + 1})
+                # We don't want the seeds to end up too close together, so make a
+                # decent range of indices around the current insert point become
+                # off-limits. The negatives shouldn't do any harm here.
+                seen_neighbor_indices.update([*range(index_to_insert_at-25, index_to_insert_at),
+                    *range(index_to_insert_at, index_to_insert_at + 25)])
                 all_gathered_text_blobs.insert(index_to_insert_at, seed_sentence)
-            #for seed_sentence in FORCED_SEEDS:
-            #    all_gathered_text_blobs.append(seed_sentence)
-            #    #corpuses_by_subreddit_by_subject[subject][sub] += f"{seed_sentence} "
             
             sub_corpus = " ".join(all_gathered_text_blobs)
-            import pdb; pdb.set_trace() 
             sub_markovifier = markovify.Text(
-                #corpuses_by_subreddit_by_subject[subject][sub], state_size=STATE_SIZE
                 sub_corpus, state_size=STATE_SIZE
             )
-
             # Just generate sentences until we get one with desired start, if possible
             try:
                 sentence = sub_markovifier.make_sentence_with_start(
